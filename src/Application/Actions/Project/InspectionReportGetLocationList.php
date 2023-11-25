095e702c762334c41b135d790804eb64b174b9e1
NIPA-LT0053 <nipa-lt0053@NIPA-LT0053s-MacBook-Air.local>
2023-11-25 21:35:06 +0700

    แก้ไข report ดึงค่า location system ไม่ได้

--- src/Application/Actions/Project/InspectionReportGetLocationList.php
+++ src/Application/Actions/Project/InspectionReportGetLocationList.php
@@ -20,119 +20,95 @@ class InspectionReportGetLocationList extends MainAction
 
         $pdo = $this->pdoConnect($_ENV['DB_PORTAL']);
 
-        $sqlQuery =
-            "SELECT 
-                    il.inspection_id,
-                    il.location_id,
-                    il.location_name,
-                    GROUP_CONCAT(
-                        JSON_OBJECT(
-                            'id', ild.id,
-                            'image_id', ild.image_id,
-                            'image_name', isto.image_name,
-                            'image_path', isto.image_path,
-                            'image_size', isto.image_size,
-                            'image_uploaded_at', isto.uploaded_at,
-                            'image_uploaded_by', JSON_OBJECT(
-                                'avatar_path', image_updated_by.avatar_path,
-                                'first_name', image_updated_by.first_name,
-                                'last_name', image_updated_by.last_name,
-                                'code_name', image_updated_by.code_name
-                            ),
-                            'deflect_status', ild.deflect_status,
-                            'deflect_detail', ild.deflect_detail,
-                            'created_at', ild.created_at,
-                            'created_by', JSON_OBJECT(
-                                'avatar_path', deflect_created_by.avatar_path,
-                                'first_name', deflect_created_by.first_name,
-                                'last_name', deflect_created_by.last_name,
-                                'code_name', deflect_created_by.code_name
-                            ),
-                            'updated_at', ild.updated_at,
-                            'updated_by', JSON_OBJECT(
-                                'avatar_path', deflect_updated_by.avatar_path,
-                                'first_name', deflect_updated_by.first_name,
-                                'last_name', deflect_updated_by.last_name,
-                                'code_name', deflect_updated_by.code_name
-                            ),
-                            'update_status_at', ild.update_status_at,
-                            'update_status_by', JSON_OBJECT(
-                                'avatar_path', deflect_update_status_by.avatar_path,
-                                'first_name', deflect_update_status_by.first_name,
-                                'last_name', deflect_update_status_by.last_name,
-                                'code_name', deflect_update_status_by.code_name
-                            )
-                        )
-                    ) AS deflect_list
-                    FROM inspection_location il
-                    LEFT JOIN inspection_location_deflect ild ON il.location_id = ild.location_id
-                    LEFT JOIN inspection_storage isto ON ild.image_id = isto.image_id
-                    LEFT JOIN member_info deflect_created_by ON ild.created_by = deflect_created_by.account_id
-                    LEFT JOIN member_info deflect_updated_by ON ild.updated_by = deflect_updated_by.account_id
-                    LEFT JOIN member_info image_updated_by ON isto.uploaded_by = image_updated_by.account_id
-                    LEFT JOIN member_info deflect_update_status_by ON ild.update_status_by = deflect_update_status_by.account_id
-                    WHERE il.project_id = :project_id AND il.inspection_id = :inspection_id
-                    GROUP BY il.inspection_id, il.location_id, il.location_name";
+        $locationQuery = "SELECT inspection_id, location_id, location_name
+                          FROM inspection_location
+                          WHERE project_id = :project_id AND inspection_id = :inspection_id";
 
-        $stmt = $pdo->prepare($sqlQuery);
-        $stmt->bindValue(':project_id', $input['project_id']);
-        $stmt->bindValue(':inspection_id', $input['inspection_id']);
-        $result = $stmt->execute();
+        $locationStmt = $pdo->prepare($locationQuery);
+        $locationStmt->bindValue(':project_id', $input['project_id']);
+        $locationStmt->bindValue(':inspection_id', $input['inspection_id']);
+        $locationStmt->execute();
 
-        if (!$result) return $this->respondWithData("Get Fail", 404);
+        $locationDataList = $locationStmt->fetchAll();
 
-        $allData = $stmt->fetchAll();
+        $deflectQuery = "SELECT 
+                            id, 
+                            location_id,
+                            image_id, 
+                            deflect_status,
+                            deflect_detail,
+                            created_at,
+                            created_by,
+                            updated_at,
+                            updated_by,
+                            update_status_at,
+                            update_status_by
+                        FROM inspection_location_deflect
+                        WHERE location_id = :location_id";
 
-        $jsonData = [];
-        foreach ($allData as $data) {
-            $deflectList = json_decode('[' . $data["deflect_list"] . ']', true);
+        $deflectStmt = $pdo->prepare($deflectQuery);
 
-            $jsonData[] = [
-                "inspection_id" => $data["inspection_id"],
-                "location_id" => $data["location_id"],
-                "location_name" => $data["location_name"],
-                "deflect_list" => array_map(function ($deflectItem) {
-                    return [
-                        'id' => $deflectItem['id'],
-                        'image_id' => $deflectItem['image_id'],
-                        'image_name' => $deflectItem['image_name'],
-                        'image_path' => $deflectItem['image_path'],
-                        'image_size' => $deflectItem['image_size'],
-                        'image_uploaded_at' => $deflectItem['image_uploaded_at'],
-                        'image_uploaded_by' => [
-                            'avatar_path' => $deflectItem['image_uploaded_by']['avatar_path'],
-                            'first_name' => $deflectItem['image_uploaded_by']['first_name'],
-                            'last_name' => $deflectItem['image_uploaded_by']['last_name'],
-                            'code_name' => $deflectItem['image_uploaded_by']['code_name'],
-                        ],
-                        'deflect_status' => $deflectItem['deflect_status'],
-                        'deflect_detail' => $deflectItem['deflect_detail'],
-                        'created_at' => $deflectItem['created_at'],
-                        'created_by' => [
-                            'avatar_path' => $deflectItem['created_by']['avatar_path'],
-                            'first_name' => $deflectItem['created_by']['first_name'],
-                            'last_name' => $deflectItem['created_by']['last_name'],
-                            'code_name' => $deflectItem['created_by']['code_name'],
-                        ],
-                        'updated_at' => $deflectItem['updated_at'],
-                        'updated_by' => [
-                            'avatar_path' => $deflectItem['updated_by']['avatar_path'],
-                            'first_name' => $deflectItem['updated_by']['first_name'],
-                            'last_name' => $deflectItem['updated_by']['last_name'],
-                            'code_name' => $deflectItem['updated_by']['code_name'],
-                        ],
-                        'update_status_at' => $deflectItem['update_status_at'],
-                        'update_status_by' => [
-                            'avatar_path' => $deflectItem['update_status_by']['avatar_path'],
-                            'first_name' => $deflectItem['update_status_by']['first_name'],
-                            'last_name' => $deflectItem['update_status_by']['last_name'],
-                            'code_name' => $deflectItem['update_status_by']['code_name'],
-                        ],
-                    ];
-                }, $deflectList),
+        $memberQuery = "SELECT 
+                            account_id, 
+                            avatar_path,
+                            code_name, 
+                            first_name,
+                            last_name
+                        FROM member_info
+                        WHERE account_id = :account_id";
+
+        $memberStmt = $pdo->prepare($memberQuery);
+
+        $combinedDataList = [];
+
+        foreach ($locationDataList as $locationData) {
+            $deflectStmt->bindValue(':location_id', $locationData['location_id']);
+            $deflectStmt->execute();
+
+            $deflectDataList = $deflectStmt->fetchAll();
+
+            foreach ($deflectDataList as &$deflectData) {
+                $memberStmt->bindValue(':account_id', $deflectData['created_by']);
+                $memberStmt->execute();
+                $createdByData = $memberStmt->fetch();
+                $deflectData['created_by'] = $createdByData;
+
+                $memberStmt->bindValue(':account_id', $deflectData['updated_by']);
+                $memberStmt->execute();
+                $updatedByData = $memberStmt->fetch();
+                $deflectData['updated_by'] = $updatedByData;
+
+                $memberStmt->bindValue(':account_id', $deflectData['update_status_by']);
+                $memberStmt->execute();
+                $updateStatusByData = $memberStmt->fetch();
+                $deflectData['update_status_by'] = $updateStatusByData;
+
+                $imageQuery = "SELECT 
+                                    image_name,
+                                    image_path, 
+                                    image_size
+                                FROM inspection_storage
+                                WHERE image_id = :image_id";
+
+                $imageStmt = $pdo->prepare($imageQuery);
+                $imageStmt->bindValue(':image_id', $deflectData['image_id']);
+                $imageStmt->execute();
+
+                $imageData = $imageStmt->fetch();
+
+                $deflectData['image_name'] = $imageData['image_name'];
+                $deflectData['image_path'] = $imageData['image_path'];
+                $deflectData['image_size'] = $imageData['image_size'];
+            }
+
+            $combinedDataList[] = [
+                'deflect_list' => $deflectDataList,
+                'inspection_id' => $locationData['inspection_id'],
+                'location_id' => $locationData['location_id'],
+                'location_name' => $locationData['location_name'],
             ];
         }
 
-        return $this->respondWithData($jsonData);
+        return $this->respondWithData($combinedDataList);
     }
 }
--- src/Application/Actions/Project/InspectionReportGetSystemList.php
+++ src/Application/Actions/Project/InspectionReportGetSystemList.php
@@ -20,130 +20,95 @@ class InspectionReportGetSystemList extends MainAction
 
         $pdo = $this->pdoConnect($_ENV['DB_PORTAL']);
 
-        $sqlQuery = "SELECT 
-                        isy.inspection_id,
-                        isy.system_id,
-                        isy.system_name,
-                        GROUP_CONCAT(
-                            JSON_OBJECT(
-                                'id', isd.id,
-                                'image_id', isd.image_id,
-                                'image_name', isto.image_name,
-                                'image_path', isto.image_path,
-                                'image_size', isto.image_size,
-                                'image_uploaded_at', isto.uploaded_at,
-                                'image_uploaded_by', JSON_OBJECT(
-                                    'avatar_path', image_updated_by.avatar_path,
-                                    'first_name', image_updated_by.first_name,
-                                    'last_name', image_updated_by.last_name,
-                                    'code_name', image_updated_by.code_name
-                                ),
-                                'deflect_status', isd.deflect_status,
-                                'deflect_detail', isd.deflect_detail,
-                                'created_at', isd.created_at,
-                                'created_by', JSON_OBJECT(
-                                    'avatar_path', deflect_created_by.avatar_path,
-                                    'first_name', deflect_created_by.first_name,
-                                    'last_name', deflect_created_by.last_name,
-                                    'code_name', deflect_created_by.code_name
-                                ),
-                                'updated_at', isd.updated_at,
-                                'updated_by', JSON_OBJECT(
-                                    'avatar_path', deflect_updated_by.avatar_path,
-                                    'first_name', deflect_updated_by.first_name,
-                                    'last_name', deflect_updated_by.last_name,
-                                    'code_name', deflect_updated_by.code_name
-                                ),
-                                'update_status_at', isd.update_status_at,
-                                'update_status_by', JSON_OBJECT(
-                                    'avatar_path', deflect_update_status_by.avatar_path,
-                                    'first_name', deflect_update_status_by.first_name,
-                                    'last_name', deflect_update_status_by.last_name,
-                                    'code_name', deflect_update_status_by.code_name
-                                )
-                            )
-                        ) AS deflect_list
-                        FROM inspection_system isy
-                        LEFT JOIN inspection_system_deflect isd ON isy.system_id = isd.system_id
-                        LEFT JOIN inspection_storage isto ON isd.image_id = isto.image_id
-                        LEFT JOIN member_info deflect_created_by ON isd.created_by = deflect_created_by.account_id
-                        LEFT JOIN member_info deflect_updated_by ON isd.updated_by = deflect_updated_by.account_id
-                        LEFT JOIN member_info image_updated_by ON isto.uploaded_by = image_updated_by.account_id
-                        LEFT JOIN member_info deflect_update_status_by ON isd.update_status_by = deflect_update_status_by.account_id
-                        WHERE isy.project_id = :project_id AND isy.inspection_id = :inspection_id
-                        GROUP BY 
-                            isy.inspection_id, 
-                            isy.system_id, 
-                            isy.system_name";
+        $systemQuery = "SELECT inspection_id, system_id, system_name
+                          FROM inspection_system
+                          WHERE project_id = :project_id AND inspection_id = :inspection_id";
 
-        $stmt = $pdo->prepare($sqlQuery);
-        $stmt->bindValue(':project_id', $input['project_id']);
-        $stmt->bindValue(':inspection_id', $input['inspection_id']);
-        $result = $stmt->execute();
+        $systemStmt = $pdo->prepare($systemQuery);
+        $systemStmt->bindValue(':project_id', $input['project_id']);
+        $systemStmt->bindValue(':inspection_id', $input['inspection_id']);
+        $systemStmt->execute();
 
-        if (!$result) return $this->respondWithData("Get Fail", 404);
+        $systemDataList = $systemStmt->fetchAll();
 
-        $allData = $stmt->fetchAll();
+        $deflectQuery = "SELECT 
+                            id, 
+                            system_id,
+                            image_id, 
+                            deflect_status,
+                            deflect_detail,
+                            created_at,
+                            created_by,
+                            updated_at,
+                            updated_by,
+                            update_status_at,
+                            update_status_by
+                        FROM inspection_system_deflect
+                        WHERE system_id = :system_id";
 
-        $jsonData = [];
-        foreach ($allData as $data) {
-            if (!empty($data["deflect_list"])) {
-                $deflectList = json_decode('[' . $data["deflect_list"] . ']', true);
+        $deflectStmt = $pdo->prepare($deflectQuery);
 
-                $jsonData[] = [
-                    "inspection_id" => $data["inspection_id"],
-                    "system_id" => $data["system_id"],
-                    "system_name" => $data["system_name"],
-                    "deflect_list" => array_map(function ($deflectItem) {
-                        return [
-                            'id' => $deflectItem['id'],
-                            'image_id' => $deflectItem['image_id'],
-                            'image_name' => $deflectItem['image_name'],
-                            'image_path' => $deflectItem['image_path'],
-                            'image_size' => $deflectItem['image_size'],
-                            'image_uploaded_at' => $deflectItem['image_uploaded_at'],
-                            'image_uploaded_by' => [
-                                'avatar_path' => $deflectItem['image_uploaded_by']['avatar_path'],
-                                'first_name' => $deflectItem['image_uploaded_by']['first_name'],
-                                'last_name' => $deflectItem['image_uploaded_by']['last_name'],
-                                'code_name' => $deflectItem['image_uploaded_by']['code_name'],
-                            ],
-                            'deflect_status' => $deflectItem['deflect_status'],
-                            'deflect_detail' => $deflectItem['deflect_detail'],
-                            'created_at' => $deflectItem['created_at'],
-                            'created_by' => [
-                                'avatar_path' => $deflectItem['created_by']['avatar_path'],
-                                'first_name' => $deflectItem['created_by']['first_name'],
-                                'last_name' => $deflectItem['created_by']['last_name'],
-                                'code_name' => $deflectItem['created_by']['code_name'],
-                            ],
-                            'updated_at' => $deflectItem['updated_at'],
-                            'updated_by' => [
-                                'avatar_path' => $deflectItem['updated_by']['avatar_path'],
-                                'first_name' => $deflectItem['updated_by']['first_name'],
-                                'last_name' => $deflectItem['updated_by']['last_name'],
-                                'code_name' => $deflectItem['updated_by']['code_name'],
-                            ],
-                            'update_status_at' => $deflectItem['update_status_at'],
-                            'update_status_by' => [
-                                'avatar_path' => $deflectItem['update_status_by']['avatar_path'],
-                                'first_name' => $deflectItem['update_status_by']['first_name'],
-                                'last_name' => $deflectItem['update_status_by']['last_name'],
-                                'code_name' => $deflectItem['update_status_by']['code_name'],
-                            ],
-                        ];
-                    }, $deflectList),
-                ];
-            } else {
-                $jsonData[] = [
-                    "inspection_id" => $data["inspection_id"],
-                    "system_id" => $data["system_id"],
-                    "system_name" => $data["system_name"],
-                    "deflect_list" => []
-                ];
+        $memberQuery = "SELECT 
+                            account_id, 
+                            avatar_path,
+                            code_name, 
+                            first_name,
+                            last_name
+                        FROM member_info
+                        WHERE account_id = :account_id";
+
+        $memberStmt = $pdo->prepare($memberQuery);
+
+        $combinedDataList = [];
+
+        foreach ($systemDataList as $systemData) {
+            $deflectStmt->bindValue(':system_id', $systemData['system_id']);
+            $deflectStmt->execute();
+
+            $deflectDataList = $deflectStmt->fetchAll();
+
+            foreach ($deflectDataList as &$deflectData) {
+                $memberStmt->bindValue(':account_id', $deflectData['created_by']);
+                $memberStmt->execute();
+                $createdByData = $memberStmt->fetch();
+                $deflectData['created_by'] = $createdByData;
+
+                $memberStmt->bindValue(':account_id', $deflectData['updated_by']);
+                $memberStmt->execute();
+                $updatedByData = $memberStmt->fetch();
+                $deflectData['updated_by'] = $updatedByData;
+
+                $memberStmt->bindValue(':account_id', $deflectData['update_status_by']);
+                $memberStmt->execute();
+                $updateStatusByData = $memberStmt->fetch();
+                $deflectData['update_status_by'] = $updateStatusByData;
+
+                $imageQuery = "SELECT 
+                                    image_name,
+                                    image_path, 
+                                    image_size
+                                FROM inspection_storage
+                                WHERE image_id = :image_id";
+
+                $imageStmt = $pdo->prepare($imageQuery);
+                $imageStmt->bindValue(':image_id', $deflectData['image_id']);
+                $imageStmt->execute();
+
+                $imageData = $imageStmt->fetch();
+
+                $deflectData['image_name'] = $imageData['image_name'];
+                $deflectData['image_path'] = $imageData['image_path'];
+                $deflectData['image_size'] = $imageData['image_size'];
             }
+
+            $combinedDataList[] = [
+                'deflect_list' => $deflectDataList,
+                'inspection_id' => $systemData['inspection_id'],
+                'system_id' => $systemData['system_id'],
+                'system_name' => $systemData['system_name'],
+            ];
         }
 
-        return $this->respondWithData($jsonData);
+        return $this->respondWithData($combinedDataList);
     }
 }
